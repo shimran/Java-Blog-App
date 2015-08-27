a90a07f7d5b23db73504822aa9582c3dbbde20fa
Shimran George <shimran@nyu.edu>
2015-08-27 00:49:51 -0700

    Add template with velocity engine

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,7 +1,16 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class app {
   public static void main(String[] args) {
-     get("/hello", (request, response) -> "Welcome to ShimCity!");
-  }
+    staticFileLocation("/public");
+    get("/", (request, response) -> {
+              return new ModelAndView(new HashMap(), "templates/hello.vtl");
+        }, new VelocityTemplateEngine());
+
+ }
 }
